609f4a19c7aafa18f702f4026b1ffe591aeb8eb6
Ioannis Filippidis <jfilippidis@gmail.com>
2015-04-08 02:55:40 -0700

    update test to use AST class container

--- tests/bitvector_test.py
+++ tests/bitvector_test.py
@@ -342,7 +342,7 @@ def test_prefix_parser():
 
 def test_flatten_memory_nodes():
     # buffer
-    nodes = openpromela.bdd._make_memory_nodes()
+    nodes = openpromela.bdd.MemoryNodes()
     x = nodes.Var('x')
     mem = [x, x, x]
     b = nodes.Buffer(mem)
