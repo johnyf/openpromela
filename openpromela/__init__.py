6de847e883e386b6fe6e6ea3f42d8659d8c207f3
Ioannis Filippidis <jfilippidis@gmail.com>
2015-04-14 02:23:31 -0700

    hide `version.py`

--- openpromela/__init__.py
+++ openpromela/__init__.py
@@ -1,5 +1,5 @@
 """Synthesizer of Mealy transducers from open Promela."""
 try:
-    from .version import version as __version__
+    from ._version import version as __version__
 except:
     __version__ = None
