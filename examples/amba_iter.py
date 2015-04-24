99c50e854dcb9ad6792f01be70c27d01c8d4ab86
Ioannis Filippidis <jfilippidis@gmail.com>
2015-04-23 19:11:34 -0700

    fixup: extend and improve AMBA experiments script

--- examples/amba_iter.py
+++ examples/amba_iter.py
@@ -64,6 +64,11 @@ if __name__ == '__main__':
         sh = logging.StreamHandler()
         logger.addHandler(sh)
         logger.setLevel(logging.DEBUG)
+    all_vars = list()
+    env_vars = list()
+    strategy_sizes = list()
+    total_time = list()
+    realizability_time = list()
     for i in xrange(n, m):
         # check if other users
         users = psutil.get_users()
@@ -82,9 +87,14 @@ if __name__ == '__main__':
         # input_file = 'amba_{i}_merged.txt'.format(i=i)
         with open(input_file) as f:
             s = f.read()
-        # print('drop caches...')
-        # this avoids creating log files as root
-        # os.system('sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"')
+        # prep input
+        j = i - 1
+        newline = '#define N {j}'.format(j=j)
+        code = re.sub('#define N.*', newline, s)
+        logger.debug(code)
+        # add multiple weak fairness assumptions
+        # c += form_progress(i)
+        # log and strategy file names
         bdd_file = 'bdd_{i}_masters.txt'.format(i=i)
         time_file = 'log_{i}_masters.txt'.format(i=i)
         logged_details_file = 'slugs_details.txt'
@@ -95,17 +105,9 @@ if __name__ == '__main__':
             fh_time = logging.FileHandler(time_file)
             log = logging.getLogger('openpromela.slugs.slugs')
             log.addHandler(fh_time)
-            # assign log file for fixed point computation details
-            # prep input
-            j = i - 1
-            newline = '#define N {j}'.format(j=j)
-            c = re.sub('#define N.*', newline, s)
-            logger.debug(c)
-            # add multiple weak fairness assumptions
-            # c += form_progress(i)
             # run
             t0 = time.time()
-            r = logic.synthesize(c, symbolic=True, bddfile=bdd_file)
+            r = logic.synthesize(code, symbolic=True, bddfile=bdd_file)
             t1 = time.time()
             dt = datetime.timedelta(seconds=t1 - t0)
             # close log files
@@ -113,7 +115,7 @@ if __name__ == '__main__':
             log.removeHandler(fh_time)
             fh_time.close()
             assert r is not None, 'NOT REALISABLE !!!'
-            print('Done with {i} masters in {dt}.'.format(i=i, dt=dt))
+            print('Done synthesizing {i} masters in {dt}.'.format(i=i, dt=dt))
             # copy log file
             shutil.copy(logged_details_file, details_file)
         # parse and plot the details
@@ -201,7 +203,8 @@ if __name__ == '__main__':
         times = np.array(times, dtype=float)
         strategy_time = np.array(strategy_time, dtype=float) + times[-1]
         conj_time = np.array(conj_time, dtype=float) + times[-1]
-        # print list(zip(times, reorder, sys, env, nodes, x_nodes, y_nodes, z_nodes))
+        # print list(zip(times, reorder, sys, env,
+        #            nodes, x_nodes, y_nodes, z_nodes))
         n = 7
         fig = plt.figure()
         fig.set_size_inches(5, 10)
@@ -214,64 +217,138 @@ if __name__ == '__main__':
         plt.plot([strategy_time[-1], conj_time[0]],
                  [strategy_reorder[-1], conj_reorder[0]], '--')
         plt.plot(conj_time, conj_reorder)
+        plt.axvspan(strategy_time[0], strategy_time[-1],
+                    facecolor='g', alpha=0.2, edgecolor='none',
+                    zorder=10)
+        x_bisector = [0, conj_time[-1]]
+        plt.plot(x_bisector, x_bisector, 'k:')
+        plt.locator_params(nbins=4)
         plt.ylabel('Reordering (ms)')
         plt.grid()
-        ax.ticklabel_format(style='sci', scilimits=(0,0))
+        ax.ticklabel_format(style='sci', scilimits=(0, 0))
         # goals
         ax = plt.subplot(n, 1, 2)
         plt.plot(times, sys)
         plt.plot(strategy_time, strategy_goal)
         plt.plot(conj_time, conj_goal)
+        plt.axvspan(strategy_time[0], strategy_time[-1],
+                    facecolor='g', alpha=0.2, edgecolor='none',
+                    zorder=10)
         plt.ylabel('Goal $j$')
         plt.grid()
-        ax.ticklabel_format(style='sci', scilimits=(0,0))
+        ax.ticklabel_format(style='sci', scilimits=(0, 0))
         # assumptions
         ax = plt.subplot(n, 1, 3)
         plt.plot(times, env)
         plt.plot(conj_time, 0 * conj_time)
+        plt.axvspan(strategy_time[0], strategy_time[-1],
+                    facecolor='g', alpha=0.2, edgecolor='none',
+                    zorder=10)
         plt.ylabel('Assumption $i$')
         plt.grid()
-        ax.ticklabel_format(style='sci', scilimits=(0,0))
+        ax.ticklabel_format(style='sci', scilimits=(0, 0))
         # total number of nodes
         ax = plt.subplot(n, 1, 4)
         plt.plot(times, nodes)
         plt.plot(strategy_time, strategy_nodes)
         plt.plot([strategy_time[-1], conj_time[0]],
                  [strategy_nodes[-1], conj_nodes[0]], '--')
+        plt.axvspan(strategy_time[0], strategy_time[-1],
+                    facecolor='g', alpha=0.2, edgecolor='none',
+                    zorder=10)
         plt.plot(conj_time, conj_nodes)
         plt.ylabel('Total BDD Nodes')
         plt.grid()
-        ax.ticklabel_format(style='sci', scilimits=(0,0))
+        ax.ticklabel_format(style='sci', scilimits=(0, 0))
         # nodes for each fixed point set
         ax = plt.subplot(n, 1, 5)
-        plt.plot(times, x_nodes, label='$X$')
-        plt.plot(times, y_nodes, label='$Y$')
-        plt.plot(times, z_nodes, label='$Z$')
-        plt.legend(bbox_to_anchor=(0.8, 1.1),
-                   loc='upper center', fontsize=5, ncol=3)
+        plt.plot(times, x_nodes, 'b--', label='$X$')
+        line, = plt.plot(times, y_nodes, 'g-.', label='$Y$')
+        line.set_dashes([8, 4, 2, 4, 2, 4])
+        plt.plot(times, z_nodes, 'r-', label='$Z$')
+        plt.legend(bbox_to_anchor=(0.3, 1.1),
+                   loc='upper center', fontsize=5, ncol=3,
+                   handlelength=5)
         plt.ylabel('Fixed points\n(BDD nodes)')
         plt.grid()
-        ax.ticklabel_format(style='sci', scilimits=(0,0))
+        ax.ticklabel_format(style='sci', scilimits=(0, 0))
         # individual strategies
         ax = plt.subplot(n, 1, 6)
-        # plt.plot(times, 0 * times)
-        # plt.plot(conj_time, 0 * conj_time)
-        plt.plot(strategy_time, strategy_strategy, label='$i^{th}$ strategy')
-        plt.plot(strategy_time, strategy_cases, label='cases')
-        plt.plot(strategy_time, strategy_new_cases, label='new cases')
+        plt.plot(strategy_time, strategy_strategy,
+                 'b-', label='$i^{th}$ strategy')
+        plt.plot(strategy_time, strategy_cases,
+                 'g--', label='cases')
+        plt.plot(strategy_time, strategy_new_cases,
+                 'r-.', label='new cases')
         plt.ylabel('Individual strategies\n(BDD nodes)')
         plt.grid()
         plt.legend(bbox_to_anchor=(0.5, 1.15),
                    loc='upper center', fontsize=5, ncol=3)
         ax.set_yscale('log')
-        # ax.ticklabel_format(style='sci', scilimits=(0,0))
+        # ax.ticklabel_format(style='sci', scilimits=(0, 0))
         # combined strategy
         ax = plt.subplot(n, 1, 7)
-        #plt.plot(times, 0 * times)
-        plt.plot(conj_time, conj_strategy, 'o', label='combined')
+        plt.plot(conj_time, conj_strategy, 'o--', label='combined')
+        plt.xlabel('time (milliseconds)')
         plt.ylabel('Combined strategy\n(BDD nodes)')
         plt.grid()
-        ax.ticklabel_format(style='sci', scilimits=(0,0))
+        ax.ticklabel_format(style='sci', scilimits=(0, 0))
         # save
         fname = 'details_{i}.pdf'.format(i=i)
         plt.savefig(fname, bbox_inches='tight')
+        print('Done ploting {i} masters\n'.format(i=i))
+        # accumulate results for ploting vs #masters
+        #
+        # TODO: fix to use adapted strategy,
+        # even if not synthesizing
+        aut = logic.compile_spec(code, strict_atomic=True)
+        # var numbers
+        e = {bit for bit, d in aut.vars.iteritems()
+             if d['owner'] == 'env'}
+        all_vars.append(len(aut.vars))
+        env_vars.append(len(e))
+        # strategy size
+        strategy_sizes.append(conj_strategy[-1])
+        # times
+        total_time.append(conj_time[-1])
+        realizability_time.append(times[-1])
+    all_vars = np.array(all_vars)
+    env_vars = np.array(env_vars)
+    strategy_sizes = np.array(strategy_sizes)
+    total_time = np.array(total_time)
+    realizability_time = np.array(realizability_time)
+    # plot collected results vs #masters
+    masters = np.arange(args.min, args.max + 1)
+    plt.clf()
+    # numbers of variables
+    plt.subplot(4, 1, 1)
+    plt.plot(masters, all_vars, 'b-o', label='all')
+    plt.plot(masters, all_vars - env_vars, 'g-+', label='sys')
+    plt.plot(masters, env_vars, 'r--o', label='env')
+    plt.xlabel('Number of masters')  # should it be capitalized ?
+    plt.ylabel('Number of variables')
+    plt.grid()
+    plt.legend()
+    # plt.savefig('number_of_variables.pdf', bbox_inches='tight')
+    # strategy sizes (and so BDD file memory)
+    plt.subplot(4, 1, 2)
+    plt.plot(masters, strategy_sizes, 'b--o')
+    plt.xlabel('Number of masters')
+    plt.ylabel('Strategy size (BDD nodes)')
+    plt.grid()
+    # times
+    plt.subplot(4, 1, 3)
+    plt.plot(masters, total_time, '-+', label='total')
+    plt.plot(masters, realizability_time, '--o', label='realizability')
+    plt.xlabel('Number of masters')
+    plt.ylabel('Time (msec)')
+    plt.legend()
+    plt.grid()
+    # time comparison
+    ax = plt.subplot(4, 1, 4)
+    plt.plot(total_time, realizability_time, 'o')
+    plt.xlabel('Total time (msec)')
+    plt.ylabel('Realizability time (msec)')
+    plt.grid()
+    ax.set_aspect('equal')
+    plt.savefig('strategy_sizes_and_variable_bumbers.pdf', bbox_inches='tight')
