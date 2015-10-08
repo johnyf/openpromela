bbbe8fac48547e63ebc19d8ced66f12f03c1c60b
Ioannis Filippidis <jfilippidis@gmail.com>
2015-10-08 16:03:08 -0700

    install requires `dd>=0.1.3`

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ s = (
     '# This file was generated from setup.py\n'
     "version = '{version}'\n").format(version=version)
 install_requires = [
-    'dd >= 0.0.5',
+    'dd >= 0.1.3',
     'gitpython >= 1.0.1',
     'humanize >= 0.5.1',
     'natsort >= 3.5.3',
