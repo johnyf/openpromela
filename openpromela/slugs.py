johnyf/openpromela

commit 44fa74982536f7de6804658021148f12c8564489

update: []<> and <>[] for winning condition

openpromela/slugs.py
@@ -109,11 +109,11 @@ def _to_slugs(aut):
         # env
         f(aut.init['env'], 'ENV_INIT') +
         f(aut.action['env'], 'ENV_TRANS') +
-        f(aut.win['env'], 'ENV_LIVENESS') +
+        f(aut.win['<>[]'], 'ENV_LIVENESS') +
         # sys
         f(aut.init['sys'], 'SYS_INIT') +
         f(aut.action['sys'], 'SYS_TRANS') +
-        f(aut.win['sys'], 'SYS_LIVENESS'))
+        f(aut.win['[]<>'], 'SYS_LIVENESS'))
 
 
 def _slugs_str(r, name, sep='\n'):

openpromela/logic.py
@@ -2267,7 +2267,12 @@ def map_to_future(aut):
             aut.win[q], aut.vars, free_init=init_dvars)
         a.vars.update(dvars)
         a.init[q].extend(i)
-        a.win[q].extend(f)
+        if q == 'env':
+            a.win['<>[]'].extend(f)
+        elif q == 'sys':
+            a.win['[]<>'].extend(f)
+        else:
+            raise Exception('unknown case "{q}"'.format(q=q))
     return a
 
 

commit 57183776b2432fb0ea8e68e682f6c0a1bd6494bd

BUG: negate persistence to env recurrence for `slugs`
Introduced in 44fa74982536f7de6804658021148f12c8564489, which contained also the mirror error, and the two errors cancelled each other, until 6847577163290fd2738a697de5394b4b80a6e874.

openpromela/slugs.py
@@ -102,6 +102,8 @@ def _to_slugs(aut):
     print('number of unprimed bits: {n}'.format(n=len(dbits)))
     logger.debug(
         'slugs variables:\n{v}'.format(v=pprint.pformat(dbits)))
+    persistence = aut.win['<>[]']
+    recurrence = ['! {w}'.format(w=w) for w in persistence]
     f = _slugs_str
     return (
         _format_slugs_vars(dbits, 'env', 'INPUT') +
@@ -109,7 +111,7 @@ def _to_slugs(aut):
         # env
         f(aut.init['env'], 'ENV_INIT') +
         f(aut.action['env'], 'ENV_TRANS') +
-        f(aut.win['<>[]'], 'ENV_LIVENESS') +
+        f(recurrence, 'ENV_LIVENESS') +
         # sys
         f(aut.init['sys'], 'SYS_INIT') +
         f(aut.action['sys'], 'SYS_TRANS') +