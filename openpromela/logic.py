johnyf/openpromela

commit 44fa74982536f7de6804658021148f12c8564489

update: []<> and <>[] for winning condition

openpromela/logic.py
@@ -2267,7 +2267,12 @@ def map_to_future(aut):
             aut.win[q], aut.vars, free_init=init_dvars)
         a.vars.update(dvars)
         a.init[q].extend(i)
-        a.win[q].extend(f)
+        if q == 'env':
+            a.win['<>[]'].extend(f)
+        elif q == 'sys':
+            a.win['[]<>'].extend(f)
+        else:
+            raise Exception('unknown case "{q}"'.format(q=q))
     return a
 
 

openpromela/slugs.py
@@ -109,11 +109,11 @@ def _to_slugs(aut):
         # env
         f(aut.init['env'], 'ENV_INIT') +
         f(aut.action['env'], 'ENV_TRANS') +
-        f(aut.win['env'], 'ENV_LIVENESS') +
+        f(aut.win['<>[]'], 'ENV_LIVENESS') +
         # sys
         f(aut.init['sys'], 'SYS_INIT') +
         f(aut.action['sys'], 'SYS_TRANS') +
-        f(aut.win['sys'], 'SYS_LIVENESS'))
+        f(aut.win['[]<>'], 'SYS_LIVENESS'))
 
 
 def _slugs_str(r, name, sep='\n'):

commit 6847577163290fd2738a697de5394b4b80a6e874

BUG: negate persistence properties
Introduced in 44fa74982536f7de6804658021148f12c8564489.
This error has its mirror in `openpromela.slugs._to_slugs`.
The mirror passed the <>[] values as environment recurrence to `slugs`.
The result was correct, because the <>[] values were equal to the environment recurrence goals (due to the forgotten negation).
This commit turns the original omission of negation into a bug.

openpromela/logic.py
@@ -2268,7 +2268,7 @@ def map_to_future(aut):
         a.vars.update(dvars)
         a.init[q].extend(i)
         if q == 'env':
-            a.win['<>[]'].extend(f)
+            a.win['<>[]'].extend('!({w})'.format(w=w) for w in f)
         elif q == 'sys':
             a.win['[]<>'].extend(f)
         else: