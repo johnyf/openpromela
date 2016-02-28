657a26e9efacdcc6a012a0fde8e68d9ff704ac76
Ioannis Filippidis <jfilippidis@gmail.com>
2016-02-27 21:45:26 -0800

    REL: bump to `dd >= 0.3.0` in `install_requires`

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ VERSION_TEXT = (
     '# This file was generated from setup.py\n'
     "version = '{version}'\n")
 install_requires = [
-    'dd >= 0.1.3',
+    'dd >= 0.3.0',
     'networkx >= 1.9.1',
     'omega >= 0.0.3',
     'ply >= 3.4',
